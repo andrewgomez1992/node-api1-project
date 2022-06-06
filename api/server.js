0d64e9c0c9cec26a1faf2e9b574e630650787218
Andrew Gomez <drewgomez209@gmail.com>
2022-06-06 14:01:24 -0700

    latest

--- api/server.js
+++ api/server.js
@@ -50,15 +50,19 @@ server.put('/api/users/:id', (req, res) => {
     User.update(req.params.id, req.body)
         .then(result => {
             if (result == null) {
-                res.status(200).json({ message: "The user with the specified ID does not exist" })
+                res.status(404).json({ message: "The user with the specified ID does not exist" })
+                return;
+            } else if (!req.body.name || !req.body.bio) {
+                res.status(400).json({ message: "Please provide name and bio for the user" })
                 return;
             } else {
-                return res.json({ message: "Please provide name and bio for the user" })
+                res.status(200).json()
             }
         })
         .catch(result => {
-            res.status(500).json({ message: "The user information could not be modified" });
+            res.status(500).json({ message: "The user information could not be modified" })
         })
+
 })
 
 server.delete('/api/users/:id', (req, res) => {
@@ -66,8 +70,13 @@ server.delete('/api/users/:id', (req, res) => {
         .then(result => {
             if (result == null) {
                 res.status(404).json({ message: "The user with the specified ID does not exist" });
+                return;
+            } else {
+                res.json(result)
             }
-            res.status(500).json({ message: "The user information could not be modified" });
+        })
+        .catch(result => {
+            res.status(500).json({ message: "The user could not be removed" });
         })
 });
 
